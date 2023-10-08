2336d0ce89f3f2148c1a506f355070de5c06c8d7
Asaduzzaman Pavel <contact@iampavel.dev>
2023-10-08 14:29:51 +0600

    Feat(layer): Added numpad layer

--- keyboards/keychron/q3/ansi_encoder/keymaps/k1ng440/keymap.c
+++ keyboards/keychron/q3/ansi_encoder/keymaps/k1ng440/keymap.c
@@ -1,4 +1,5 @@
-/* Copyright 2022 @ Keychron (https://www.keychron.com)
+/* Copyright 2022 @ Keychron (https://keychron.com)
+ * Copyright 2023 @ Asaduzzaman Pavel <contact@iampavel.dev>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,20 +15,26 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "action_layer.h"
+#include "bitwise.h"
+#include "keycodes.h"
+#include "rgb_matrix.h"
+#include "stdio.h"
 #include QMK_KEYBOARD_H
 
 enum layers{
     MAC_BASE,
     MAC_FN,
     WIN_BASE,
-    WIN_FN
+    WIN_FN,
+    NUMPAD,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_tkl_f13_ansi(
-        KC_ESC,   KC_BRID,  KC_BRIU,  KC_NO,    KC_NO,    RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    KC_MUTE,  KC_NO,    KC_NO,    RGB_MOD,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
+        KC_ESC,   KC_BRID,  KC_BRIU,  KC_NO,    KC_NO,    RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    KC_MUTE,  KC_NO,    TG(NUMPAD),  RGB_MOD,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,     KC_PGUP,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,  KC_DEL,   KC_END,      KC_PGDN,
         KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,
         KC_LCTL,  KC_LOPT,  KC_LCMD,                                KC_SPC,                                 KC_RCMD,  KC_ROPT,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
@@ -42,13 +49,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,  _______,  _______,  _______
     ),
 
+    /*
+     * ,---------------------------------------------------------------------------------------------------------------------------.
+     * | ESC   |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | F11  | F12  | MUTE | PRINT | SCROLL | PAUSE |
+     * |-------+------+------+------+------+-------------+------+------+------+------+------+------+------+-------+--------+-------|
+     * |  ~    |  1   |  2   |  3   |  4   |  5   |  6   |  7   |  8   |  9   |  0   |  -   |   +  | BSPC | INS   | HOME   | PGUP  |
+     * |-------+------+------+------+------+-------------+------+------+------+------+------+------+------+-------+--------+-------|
+     * | TAB   |  Q   |  W   |  E   |  R   |  T   |  Y   |  U   |  I   |  O   |  P   |  [   |   ]  |  \   |  DEL  |  END   | PGDN  |
+     * |-------+------+------+------+------+-------------+------+------+------+------+------+------+------+-------+--------+-------|
+     * | ESC   |  A   |  S   |   D  |  F   |  G   |  H   |  J   |  K   |  L   |  ;   |  '   |      | ENT |                         |
+     * |-------+------+------+------+------+-------------+------+------+------+------+------+------+------+-------+--------+-------|
+     * | SFT   |  Z   |  X   |  C   |  V   |  B   |  N   |  M   |  M   |  ,<  |  .>  |  /?  |      | SFT |        |   UP   |       |
+     * |-------+------+------+------+------+-------------+------+------+------+------+------+------+------+-------+--------+-------|
+     * | CRTRL | WIN  | ALT  |             | SPC  |                    | ALT  |  WIN | FN   | CTRL |      |   L   |  DOWN  |  R    |
+     * `---------------------------------------------------------------------------------------------------------------------------'
+     */
     [WIN_BASE] = LAYOUT_tkl_f13_ansi(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_MUTE,  KC_PSCR,  KC_NO,    RGB_MOD,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_MUTE,  KC_PSCR,  TG(NUMPAD), RGB_MOD,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,    KC_PGUP,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,  KC_DEL,   KC_END,     KC_PGDN,
         KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,    KC_RGHT
     ),
 
     [WIN_FN] = LAYOUT_tkl_f13_ansi(
@@ -59,13 +81,91 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,            _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,  _______,  _______,  _______
     ),
+
+    /*
+     * ,---------------------------------------------------------------------------------------------------------------------------.
+     * | ESC   |      |      |      |      |      |      |      |      |      |      |      |      |      |       |        |       |
+     * |-------+------+------+------+------+-------------+------+------+------+------+------+------+------+-------+--------+-------|
+     * |       |  1   |  2   |  3   |  4   |  5   |  6   |  7   |  8   |  9   |  0   |  -   |  +   | BSPC |       |        |       |
+     * |-------+------+------+------+------+-------------+------+------+------+------+------+------+------+-------+--------+-------|
+     * |       |      |      |      |      |      |      |  4   |  5   |  6   |  /   |      |      |  /   |       |        |       |
+     * |-------+------+------+------+------+-------------+------+------+------+------+------+------+------+-------+--------+-------|
+     * |       |      |      |      |      |      |      |  1   |  2   |  3   |  *   |      |      |  ENT |       |        |       |
+     * |-------+------+------+------+------+-------------+------+------+------+------+------+------+------+-------+--------+-------|
+     * |       |      |      |      |      |      |      |  .   |  0   |  +   |  -   |      |      |      |       |        |       |
+     * |-------+------+------+------+------+-------------+------+------+------+------+------+------+------+-------+--------+-------|
+     * |       |      |      |      |      |      |      |      |      |      | ENT  |      |      |      |       |        |       |
+     * `---------------------------------------------------------------------------------------------------------------------------'
+     */
+    [NUMPAD] = LAYOUT_tkl_f13_ansi(
+        TG(NUMPAD),   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,     _______,  _______,  _______,  TG(NUMPAD),  _______,
+        _______,         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,      KC_0,  KC_MINS,      KC_EQL,  KC_BSPC,  _______,     _______,  _______,
+        _______,      _______,  _______,  _______,  _______,  _______,  _______,     KC_4,     KC_5,     KC_6,   KC_PSLS,  _______,     _______,  KC_PSLS,  _______,     _______,  _______,
+        _______,      _______,  _______,  _______,  _______,  _______,  _______,     KC_1,     KC_2,     KC_3,   KC_PAST,  _______,               KC_ENT,
+        _______,                _______,  _______,  _______,  _______,  _______,   KC_DOT,     KC_0,  KC_PPLS,   KC_PMNS,  _______,               _______,               _______,
+        _______,      _______,  _______,                                _______,                                  KC_ENT,  _______,  TG(NUMPAD),  _______,  _______,     _______,  _______
+
+        // TODO: Figure out why KC_KP does not work
+        // TG(NUMPAD),   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,    _______,     _______,  TG(NUMPAD),  _______,
+        // _______,      KC_KP_1,  KC_KP_2,  KC_KP_3,  KC_KP_4,  KC_KP_5,  KC_KP_6,  KC_KP_7,  KC_KP_8,  KC_KP_9,   KC_KP_0,  KC_MINS,  KC_EQL,  KC_BSPC,  _______,  _______,  _______,
+        // _______,      _______,  _______,  _______,  _______,  _______,  _______,  KC_KP_4,  KC_KP_5,  KC_KP_6,   KC_PSLS,  _______,   _______,    KC_BSLS,     _______,  _______,  _______,
+        // _______,      _______,  _______,  _______,  _______,  _______,  _______,  KC_KP_1,  KC_KP_2,  KC_KP_3,   KC_PAST,  _______,                 KC_ENT,
+        // _______,                _______,  _______,  _______,  _______,  _______,  KC_DOT,   KC_KP_0,  KC_PPLS,   KC_PMNS,  _______,                 _______,               _______,
+        // _______,      _______,  _______,                                _______,                                 KC_ENT,   _______,     TG(NUMPAD), _______,     _______,  _______,  _______
+    ),
 };
+
+// https://www.reddit.com/r/olkb/comments/kpro3p/how_to_use_layer_indicators_with_rgb_matrix_for/
+#define NUMPAD_INDICATOR_COLOR(keyIndex) rgb_matrix_set_color(keyIndex, 0, 0, 255) // blue
+bool rgb_matrix_indicators_user(void) {
+    uint8_t layer = biton32(layer_state);
+    switch (layer) {
+        case NUMPAD: // numpad layer
+            NUMPAD_INDICATOR_COLOR(0); // key = ESC
+            NUMPAD_INDICATOR_COLOR(14); // key = scroll lock
+
+            // NUMROW
+            for(int i = 16; i <= 29; ++i) {
+                NUMPAD_INDICATOR_COLOR(i);
+            }
+
+            // U I O P |
+            NUMPAD_INDICATOR_COLOR(40);
+            NUMPAD_INDICATOR_COLOR(41);
+            NUMPAD_INDICATOR_COLOR(42);
+            NUMPAD_INDICATOR_COLOR(43);
+            NUMPAD_INDICATOR_COLOR(46);
+
+            // J K L ;
+            NUMPAD_INDICATOR_COLOR(57);
+            NUMPAD_INDICATOR_COLOR(58);
+            NUMPAD_INDICATOR_COLOR(59);
+            NUMPAD_INDICATOR_COLOR(60);
+
+            // Enter
+            NUMPAD_INDICATOR_COLOR(62);
+
+            // NM,.
+            NUMPAD_INDICATOR_COLOR(69);
+            NUMPAD_INDICATOR_COLOR(70);
+            NUMPAD_INDICATOR_COLOR(71);
+            NUMPAD_INDICATOR_COLOR(72);
+
+            // RALT
+            NUMPAD_INDICATOR_COLOR(80);
+
+            break;
+    }
+
+    return true;
+}
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [MAC_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [MAC_FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
     [WIN_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [WIN_FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI) }
+    [WIN_FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
+    [NUMPAD]   = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
 };
 #endif // ENCODER_MAP_ENABLE
